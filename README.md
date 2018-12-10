675f50c0ffd677fff1f54c7448a25696f8c8fcce
Juan I Diaz <jdi@idiaz.ca>
2018-12-10 15:00:00 -0500

    Firt update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# giphy
+###
+### Project name:   Giphy
+### Cretaed by:     Juan I Diaz (jdi@idiaz.ca)
+### Date:           December 2018
+### 
+
+### DESCRIPTION
+
+
+
+#INSTRUCTIONS:
+
+
+
+
+
+# TIPS
+
+
+
+# QUESTIONS OR COMMENTS
+- Feel free to contact the developer!
---
+++ assets/javascript/giphy.js
@@ -0,0 +1,30 @@
+// VARIABLES --------------------------------------------
+
+//      OBJECTS
+
+
+
+//      ARRAYS
+
+
+
+//      STRINGS/CHAR
+
+
+
+//      NUMBER/INTEGER
+
+
+
+//      BOOLEAN
+
+
+
+// ------------------------------------------------------------
+
+$(document).ready(function () {
+
+    console.log("Yup... its loaded!");
+}
+
+
